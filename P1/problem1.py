30e67a2ba3d6993a3a91ce2d4e8dc2705cde8cc3
Dave Chuck <dczhuo1388@gmail.com>
2024-12-04 00:03:56 -0400

    problem1 p2 done

--- P1/problem1.py
+++ P1/problem1.py
@@ -1,9 +1,17 @@
+from collections import Counter
+
 def calculate_total_distance(left_list, right_list):
     left_list_sorted = sorted(left_list)
     right_list_sorted = sorted(right_list)
 
     total_distance = sum(abs(l - r) for l, r in zip(left_list_sorted, right_list_sorted))
     return total_distance
+
+
+def calculate_similarity_score(left_list, right_list):
+    right_count = Counter(right_list)
+    similarity_score = sum(num * right_count[num] for num in left_list)
+    return similarity_score
 
 
 def read_lists_from_file(file_path):
@@ -27,8 +35,11 @@ def main():
 
     # Calculate the total distance
     total_distance = calculate_total_distance(left_list, right_list)
-
     print("Total Distance:", total_distance)
+
+    # Calculate sim score
+    similarity_score = calculate_similarity_score(left_list, right_list)
+    print("Similarity Score:", similarity_score)
 
 
 if __name__ == "__main__":
